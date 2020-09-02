5190ba40ca6fb83d9e157f4f5187b6b23f26b3f9
Andriejus <a.gorbunovas@gmail.com>
2020-09-02 12:28:51 +0300

    Add Cookies notice tests

--- TesonetWebPageAutomation/cypress/integration/examples/TestTesonetComCareerPage.js
+++ TesonetWebPageAutomation/cypress/integration/examples/TestTesonetComCareerPage.js
@@ -7,6 +7,11 @@ describe('Tesonet.com Web page test', () => {
         });
         console.log('Should be printed AFTER *** PAGE LOADED ***');
     });
+    it('Cookies notice test on main page', () => {
+        cy.get('.cookie-notice-container').should('be.visible');
+        cy.get('#cn-accept-cookie').click();
+        cy.get('.cookie-notice-container').should('not.be.visible');
+    });
     it('Select QA department from dropdown list on Career page', () => {
         cy.get('#menu-item-19').click();
         cy.wait(2000);
@@ -14,6 +19,11 @@ describe('Tesonet.com Web page test', () => {
         cy.get('[data-filter-group="departments"]')
             .select('.tesonet-qa')
             .should('have.value', '.tesonet-qa');        
+    });
+    it('Cookies notice test on Career page', () => {
+        cy.get('.cookie-notice-container').should('be.visible');
+        cy.get('#cn-accept-cookie').click();
+        cy.get('.cookie-notice-container').should('not.be.visible');
     });
     it('Count the number of categories available on Career page', () => {
         cy.get('#menu-item-19').click();
